65777b4744e52732c591ee59eb95943c9529d82e
i-hossain <ridoy.hossain@mail.utoronto.ca>
2017-05-26 12:08:57 -0400

    Added email authentication with Firebase

--- SocialNetwork/LoginVC.swift
+++ SocialNetwork/LoginVC.swift
@@ -13,6 +13,9 @@ import Firebase
 
 class LoginVC: UIViewController {
     
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     
 
     override func viewDidLoad() {
@@ -35,6 +38,27 @@ class LoginVC: UIViewController {
         }
     }
     
+    @IBAction func signInBtnPressed(_ sender: Any) {
+        if let email = emailField.text, let password = passwordField.text {
+            Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+                if error == nil {
+                    print("Successfully logged in with email")
+                }
+                else {
+                    Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if error != nil {
+                            print("Could not create user \(error.debugDescription)")
+                        }
+                        else {
+                            print("Successfully created user account")
+                        }
+                    })
+                }
+            })
+        }
+    }
+    
+    
     func authenticateWithFirebase(_ credential: AuthCredential) {
         Auth.auth().signIn(with: credential) { (user, error) in
             if error != nil {
@@ -45,5 +69,7 @@ class LoginVC: UIViewController {
             }
         }
     }
+    
+    
 }
 
