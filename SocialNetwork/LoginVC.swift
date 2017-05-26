3d24f2385e0b4ca08afb63faecee4cd42ac443b4
Ismail Hossain <ridoy.hossain@mail.utoronto.ca>
2017-05-26 01:30:33 -0400

    Successfully authenticated with Firebase using Facebook

--- SocialNetwork/LoginVC.swift
+++ SocialNetwork/LoginVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FacebookCore
+import FacebookLogin
+import Firebase
 
 class LoginVC: UIViewController {
     
@@ -17,6 +20,30 @@ class LoginVC: UIViewController {
     }
 
     @IBAction func facebookBtnPressed(_ sender: Any) {
+        let loginManager = LoginManager()
+        loginManager.logIn([.publicProfile, .email], viewController: self) { (result) in
+            switch result {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                print("Logged in!")
+                let credential = FacebookAuthProvider.credential(withAccessToken: accessToken.authenticationToken)
+                self.authenticateWithFirebase(credential)
+            }
+        }
+    }
+    
+    func authenticateWithFirebase(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential) { (user, error) in
+            if error != nil {
+                print("Error: \(error.debugDescription)")
+            }
+            else {
+                print("Successfully authenticated with Firebase")
+            }
+        }
     }
 }
 
